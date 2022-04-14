863172a336df516762aef9f16e464a67f12df72e
craiit <rathnau@integrate-it.net>
2022-04-14 16:23:14 +0200

    Functions and Definitions Update

--- ps_functions/Github Management.ps1
+++ ps_functions/Github Management.ps1
@@ -11,27 +11,6 @@ function git-auto       {
 }
 
 function git-ps       {
-
-
-
-                            param([switch]$Elevated)
-                        function Test-Admin{ # https://superuser.com/questions/108207/how-to-run-a-powershell-script-as-administrator
-                        
-                        $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
-                        $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
-                        } 
-
-                        if ((Test-Admin) -eq $false)  {
-                            if ($elevated) 
-                            {
-                                # tried to elevate, did not work, aborting
-                            } 
-                            else {
-                                Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
-                        }
-
-                        exit
-                        }
                         
                         $current_path = (pwd).path
                         Set-Location $PSHOME
