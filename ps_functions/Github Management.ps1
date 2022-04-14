d3b88859cf910758b545cfc5d7a53f2ceb0b7912
craiit <rathnau@integrate-it.net>
2022-04-14 15:27:11 +0200

    Functions and Definitions Update

--- ps_functions/Github Management.ps1
+++ ps_functions/Github Management.ps1
@@ -10,6 +10,7 @@ function git-auto       {
 }
 
 function git-ps       {
+                        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
                         $current_path = (pwd).path
                         Set-Location $PSHOME
                         git add .\profile.ps1
