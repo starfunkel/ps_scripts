0bc6c5c25fb06cd79fda12a341613a136211a001
craiit <rathnau@integrate-it.net>
2022-04-26 23:14:26 +0200

    Auto-commit

--- ps_functions/ad-adm.ps1
+++ ps_functions/ad-adm.ps1
@@ -23,11 +23,11 @@
 #### AD Info on Prem
 function adu        ( $USER ) { # Get AD User Info
     Write-Host ""
-    Write-Host "---------------------------------------------------------------------------------"
+    Write-Host "---------------------------------------------------------------------------------" -InformationVariable LINEDELIMITERS
     Write-Host "User Info:"
     Get-ADUser $USER -Properties *  |
     Select-Object SamAccountName, Displayname, distinguishedname, EmailAddress, OfficePhone, Enabled, lastlogondate, LastBadpasswordAttempt, BadLogonCount, PasswordLastSet, PasswordNeverExpires,SID
-    Write-Host "---------------------------------------------------------------------------------"
+    $LINEDELIMITERS
     Write-Host ""
     Write-Host "Group memberships of $USER"
     get-adprincipalGROUPmembership -Identity $USER |
@@ -35,33 +35,33 @@ function adu        ( $USER ) { # Get AD User Info
     Format-Table
     Sort-Object name
     Write-Host ""
-    Write-Host "---------------------------------------------------------------------------------"
+    $LINEDELIMITERS
     #|
     #Format-table SamAccountName, Name, Displayname, EmailAddress, OfficePhone, Enabled, lastlogondate, LastBadpasswordAttempt, lastlogondate, PasswordLastSet, PasswordNeverExpires,SID
 }
 function adg        ( $GROUP) { # Get AD Group Info
     Write-Host ""
-    Write-Host "---------------------------------------------------------------------------------"
+    Write-Host "---------------------------------------------------------------------------------" -InformationVariable LINEDELIMITERS
     Write-Host "Group Info:" 
     get-adGROUP $GROUP -Properties * |
     Select-Object CN, DistinguishedName, SamAccountName, CanonicalName, GroupCategory, GroupScope, whenChanged, whenCreated, info, objectSid
-    Write-Host "---------------------------------------------------------------------------------"
+    $LINEDELIMITERS
     Write-Host ""
     Write-Host "Group Member Info:"
     get-adGROUPmember $GROUP -Recursive -Server vwdc |
     Get-ADUser -Properties *  |
     Select-Object SamAccountName, Displayname, Enabled, lastlogondate, LastBadpasswordAttempt, BadLogonCount, PasswordLastSet, PasswordNeverExpires, SID |
     Format-table SamAccountName, Displayname, Enabled, lastlogondate, LastBadpasswordAttempt, BadLogonCount, PasswordLastSet, PasswordNeverExpires, SID
-    Write-Host "---------------------------------------------------------------------------------"
+    $LINEDELIMITERS
     Write-Host ""
 }
 function adc        ( $COMPUTER ){ # Get Ad Computer Info
     Write-Host ""
-    Write-Host "---------------------------------------------------------------------------------"
+    Write-Host "---------------------------------------------------------------------------------" -InformationVariable LINEDELIMITERS
     Write-Host "Computer Info:"
     Get-ADComputer $COMPUTER -Properties * |
     Select-Object Name , Enabled, ObjectCategory,  CanonicalName, PrimaryGroup, OperatingSystem, OperatingSystemVersion, IPv4Address, whenCreated, LastLogonDate, whenChanged, LastBadPasswordAttempt, logonCount, objectSid
-    Write-Host "---------------------------------------------------------------------------------"
+    $LINEDELIMITERS
     Write-Host ""
 }
 
