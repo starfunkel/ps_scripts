159eb67f43b8c38a389f2fd7b8b2146c5da26d81
starfunkel <cra.memb@posteo.net>
2022-07-24 22:19:39 +0200

    Holiday PoSh desire

--- ps_functions/ad-adm.ps1
+++ ps_functions/ad-adm.ps1
@@ -20,13 +20,16 @@
 
 ### Microsoft AD Azur M365 Administration
 
-#### AD Info on Prem
+### Ad-hoc AD on prem Info
+### Gather and display AD user information
 function adu        ( $USER ) { # Get AD User Info
     Write-Host ""
     Write-Host "---------------------------------------------------------------------------------" -InformationVariable LINEDELIMITERS
     Write-Host "User Info:"
     Get-ADUser $USER -Properties *  |
-    Select-Object SamAccountName, Displayname, distinguishedname, EmailAddress, OfficePhone, Enabled, lastlogondate, LastBadpasswordAttempt, BadLogonCount, PasswordLastSet, PasswordNeverExpires,SID
+    Select-Object   SamAccountName, Displayname, distinguishedname, EmailAddress, `
+                    OfficePhone, Enabled, lastlogondate, LastBadpasswordAttempt, `
+                    BadLogonCount, PasswordLastSet, PasswordNeverExpires,SID
     $LINEDELIMITERS
     Write-Host ""
     Write-Host "Group memberships of $USER"
@@ -36,37 +39,46 @@ function adu        ( $USER ) { # Get AD User Info
     Sort-Object name
     Write-Host ""
     $LINEDELIMITERS
-    #|
-    #Format-table SamAccountName, Name, Displayname, EmailAddress, OfficePhone, Enabled, lastlogondate, LastBadpasswordAttempt, lastlogondate, PasswordLastSet, PasswordNeverExpires,SID
 }
+
+### Gather and dusplay AD group information
 function adg        ( $GROUP) { # Get AD Group Info
     Write-Host ""
     Write-Host "---------------------------------------------------------------------------------" -InformationVariable LINEDELIMITERS
     Write-Host "Group Info:" 
     get-adGROUP $GROUP -Properties * |
-    Select-Object CN, DistinguishedName, SamAccountName, CanonicalName, GroupCategory, GroupScope, whenChanged, whenCreated, info, objectSid
+    Select-Object   CN, DistinguishedName, SamAccountName, CanonicalName, GroupCategory, `
+                    GroupScope, whenChanged, whenCreated, info, objectSid
     $LINEDELIMITERS
     Write-Host ""
     Write-Host "Group Member Info:"
     get-adGROUPmember $GROUP -Recursive -Server vwdc |
     Get-ADUser -Properties *  |
-    Select-Object SamAccountName, Displayname, Enabled, lastlogondate, LastBadpasswordAttempt, BadLogonCount, PasswordLastSet, PasswordNeverExpires, SID |
-    Format-table SamAccountName, Displayname, Enabled, lastlogondate, LastBadpasswordAttempt, BadLogonCount, PasswordLastSet, PasswordNeverExpires, SID
+    Select-Object   SamAccountName, Displayname, Enabled, lastlogondate, `
+                    LastBadpasswordAttempt, BadLogonCount, PasswordLastSet,`
+                    PasswordNeverExpires, SID |
+    Format-table    SamAccountName, Displayname, Enabled, lastlogondate, `
+                    LastBadpasswordAttempt, BadLogonCount, PasswordLastSet, `
+                    PasswordNeverExpires, SID
     $LINEDELIMITERS
     Write-Host ""
 }
+
+### Gather and display AD computer information
 function adc        ( $COMPUTER ){ # Get Ad Computer Info
     Write-Host ""
     Write-Host "---------------------------------------------------------------------------------" -InformationVariable LINEDELIMITERS
     Write-Host "Computer Info:"
     Get-ADComputer $COMPUTER -Properties * |
-    Select-Object Name , Enabled, ObjectCategory,  CanonicalName, PrimaryGroup, OperatingSystem, OperatingSystemVersion, IPv4Address, whenCreated, LastLogonDate, whenChanged, LastBadPasswordAttempt, logonCount, objectSid
+    Select-Object   Name , Enabled, ObjectCategory,  CanonicalName, `
+                    PrimaryGroup, OperatingSystem, OperatingSystemVersion, IPv4Address,`
+                    whenCreated, LastLogonDate, whenChanged, LastBadPasswordAttempt,`
+                    logonCount, objectSid
     $LINEDELIMITERS
     Write-Host ""
 }
 
 ### Get Windows ACL Information  https: //exchangepedia.com/2017/11/get-file-or-FOLDER-permissions-using-powershell.html
-
 function gacl       ( $FOLDER ) { 
     (get-acl $FOLDER).access | Select-Object `
       @{Label="Identity";Expression={$_.IdentityReference}}, `
--- ps_functions/directory-jumpers.ps1
+++ ps_functions/directory-jumpers.ps1
@@ -24,10 +24,10 @@ function runners    { Set-Location C:\support\runners }
 function sup        { Set-Location c:\support\ }
 function sysint     { set-Location C:\support\Sysinternals }
 function gitd       { Set-Location C:\support\code\_git-repos }
-function HKLM:      { Set-Location HKLM: }
-function HKCU:      { Set-Location HKCU: }
-function Env:       { Set-Location Env: }
-function C:         { Set-Location C:\ }
+function HKLM       { Set-Location HKLM: }
+function HKCU       { Set-Location HKCU: }
+function Env        { Set-Location Env: }
+function C          { Set-Location C:\ }
 function ~          { Set-Location $env:UserProfile }
 function grs        { Set-Location C:\support\code\_git-repos }
 function posh       { Set-Location C:\support\code\_git-repos\POSH }
