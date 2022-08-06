922c0d05ab4e775a16f243fcb7495d781c4d1437
starfunkel <cra.memb@posteo.net>
2022-08-06 22:29:23 +0200

    vacation ending thoughts

--- ps_profile/profile.ps1
+++ ps_profile/profile.ps1
@@ -6,7 +6,7 @@ Cra's Powershell Profile
 2020 - 2022
  
 .NOTES
-All additional scripts and functions which should be loaded by default when a new ps session starts have to be placed according to line 59
+All additional scripts and functions which should be loaded by default when a new ps session initiates have to be placed to a location according to line 62.
 
 To Do:
 
@@ -14,59 +14,55 @@ Build variables for custom location folders
 
 #>
 
-Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope CurrentUser
-
-$host.ui.RawUI.WindowTitle='Black Magic'
-
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope CurrentUser # Living on the edge of things ;-)
+$host.ui.RawUI.WindowTitle='Black Magic' # Set window title
 Clear-Host
 
-<#
-Write-Host "##########################" -ForegroundColor Yellow 
-Write-Host "#                        #" -ForegroundColor Yellow
-Write-Host "#      Hello Friend!     #" -ForegroundColor Yellow
-Write-Host "#                        #" -ForegroundColor Yellow
-Write-Host "##########################" -ForegroundColor Yellow
-#>
+# Get time for prompt
+function Get-Time
+    { return $(get-date |
+        ForEach-Object { $_.ToLongTimeString() } 
+              )
+    }
 
-### Colored and timy time promt
+### Colored and tiny time prompt
 function prompt
 
-{   # Write the time and dir
-    write-host "[" -noNewLine
-    write-host $(Get-Time) -foreground yellow -noNewLine
-    write-host "] " -noNewLine
-    # Write the path
-    write-host $($(Get-Location).Path.replace($home,"~").replace("\","/")) -foreground green -noNewLine
-    write-host $(if ($nestedpromptlevel -ge 1) { '>>' }) -noNewLine
-    return "> "
-}
+    {   # Write the time and dir
+        write-host "[" -noNewLine
+        write-host $(Get-Time) -foreground yellow -noNewLine
+        write-host "] " -noNewLine
+        # Write the path
+        write-host $($(Get-Location).Path.replace($home,"~").replace("\","/")) -foreground green -noNewLine
+        write-host $(if ($nestedpromptlevel -ge 1) { '>>' }) -noNewLine
+        return "> "
+    }
 
-### Get weather
+# Get weather forecast function
 function getw       {(Invoke-WebRequest http://wttr.in/:Berlin?0M -UserAgent "curl" -ErrorAction SilentlyContinue ).Content}
-getw
 
-### Get time for prompt
-function Get-Time   { return $(get-date | ForEach-Object { $_.ToLongTimeString() } ) }
+# Handling & Moving in posh
 
-
-### Handling & Moving
-
-### Autocompletion
+# Autocompletion
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 
-### Autocompletion for arrow keys
+# Autocompletion for arrow keys
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
 
-### Import ps scripts and definitions by iterating through each folder 
+# Import ps scripts and definitions by iterating through each folder 
 $Path = "C:\support\code\_git-repos\cras_stuff\POSH\ps_functions"
-Get-ChildItem -Path $Path  -Filter *.ps1 |ForEach-Object {
-    . $_.FullName
-}
+Get-ChildItem -Path $Path  -Filter *.ps1 |
+    ForEach-Object {
+        . $_.FullName
+    }
 
-### Set Custom PS module directory
+# Set Custom PS module directory
 $env:PSModulePath = ((@("C:\support\code\_git-repos\cras_stuff\POSH\ps-modules") + ($env:PSModulePath -split ";")) -join ";")
 
-### Setting Start Dir
+# Start with this:
+getw
+
+# Setting Start Dir
 Set-Location c:\support
 
