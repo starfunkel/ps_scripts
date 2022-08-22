ec5e6f5dead725df7c8bf717d3902b3822ae2f2e
craiit <rathnau@integrate-it.net>
2022-08-22 16:27:22 +0200

    Auto-commit

--- ps_functions/directory-jumpers.ps1
+++ ps_functions/directory-jumpers.ps1
@@ -3,15 +3,15 @@
 function runners    { Set-Location C:\support\runners }
 function sup        { Set-Location c:\support\ }
 function sysint     { set-Location C:\support\Sysinternals }
-function gitd       { Set-Location C:\support\code\_git-repos }
+
 function HKLM       { Set-Location HKLM: }
 function HKCU       { Set-Location HKCU: }
 function Env        { Set-Location Env: }
 function C          { Set-Location C:\ }
 function ~          { Set-Location $env:UserProfile }
-function grs        { Set-Location C:\support\code\_git-repos }
-function posh       { Set-Location C:\support\code\_git-repos\POSH }
-function scrpts     { Set-Location C:\support\code\_git-repos\POSH\ps-scripts }
+
+function posh       { Set-Location C:\support\code\_git-repos\cras_stuff\POSH }
+function gitd       { Set-Location C:\support\code\_git-repos }
 
 ### Directoriey movement
 
