090f4d443c7149b1d75cbbddc1ceb1697c7ebf3b
craiit <rathnau@integrate-it.net>
2022-09-09 11:49:49 +0200

    Auto-commit

--- ps_functions/Functions.ps1
+++ ps_functions/Functions.ps1
@@ -5,7 +5,6 @@ function md5        { Get-FileHash -Algorithm MD5 $args }
 function sha1       { Get-FileHash -Algorithm SHA1 $args }
 function sha256     { Get-FileHash -Algorithm SHA256 $args }
 
-
 ### Bash like commands
 function pwd        { Get-Location }
 function ll         { Get-Childitem | Sort-Object -Property LastWriteTime }
@@ -14,20 +13,16 @@ function man        { get-help $args[0] -Full | out-host -paging }
 function cat        { get-content $args[0] }
 function less       { get-content $args[0] | out-host -paging }
 function find       { Get-Childitem -Filter $args[0] -Recurse -File | out-host -paging }
+function sudo       {Start-Process -verb RunAs wt}
+function history    { get-history -Count 20 }
+function touch      { New-Item $args[0] }
+function zip        { Compress-Archive -Path $args[0] -DestinationPath $args[1] }
+function unzip      { Expand-Archive -LiteralPath $args[0] -DestinationPath $args[1] }
 
 function uname      { $PROPERTIES = 'Caption', 'CSName', 'Version', 'BuildType', 'OSArchitecture'; Get-CimInstance Win32_OperatingSystem | 
                         Select-Object $PROPERTIES |
                         Format-Table -AutoSize 
                     }
-
-function sudo       {Start-Process -verb RunAs wt}
-                    
-function history    { get-history -Count 20 }
-
-function touch      { New-Item $args[0] }
-
-### Windows Terminal as Admin
-function aterm      { Start-Process wt.exe -Verb runAs }
 
 ### Custom functions
 function pong       { Test-Connection $args[0] | Format-Table -Autosize } 
@@ -35,7 +30,6 @@ function imp        { Import-Module $args[0] }
 
 ### Scripts
 function mtr        { C:\support\code\_git-repos\cras_stuff\POSH\ps-scripts\mtr.ps1 $args[0] }
-function gpa        { C:\support\code\_git-repos\cras_stuff\POSH\ps-scripts\GPAnalyser.ps1 }
 
 ### Tools
 function lgpo       { C:\support\runners\lgpo.exe }
@@ -53,24 +47,6 @@ function ffp        { & "$env:ProgramFiles\Mozilla Firefox\firefox.exe" -p }
 ### Reload Powershell Profile
 function rel        { & powershell}
 
-<#
-# alternative Reload profile https: //itenium.be/blog/dev-setup/powershell-profiles/
-function rel        {
-                      @(
-                          $Profile.AllUsersAllHosts,
-                          $Profile.AllUsersCurrentHost,
-                          $Profile.CurrentUserAllHosts,
-                          $Profile.CurrentUserCurrentHost
-                      ) | 
-                      ForEach-Object {
-                          if (Test-Path $_) {
-                              Write-Verbose "Reloading $_"
-                              . $_
-                          }
-                      }
-      }
-#>
-
 function ip         { $env:externalip = ( # gets external $ internal IPs of Localhost
                         Invoke-WebRequest "ifconfig.me/ip").Content;
                         $LOCALIPADRESS = ((ipconfig | findstr [0-9].\.)[0]).Split()[-1]
@@ -83,15 +59,8 @@ function ip         { $env:externalip = ( # gets external $ internal IPs of Loca
                         Write-host ""
                     }
 
-### Start python 
-function py         { & "C:\Program Files\WindowsApps\PythonSoftwareFoundation.Python.3.10_3.10.1520.0_x64__qbz5n2kfra8p0\python3.10.exe" }
-
 ### cowsay stuff
 function cow        { Get-Cowsay "$args" } # Install-Module -Name CowsaySharp
 function tux        { Get-Cowsay -cowfile tux "$args" }
 
-### Directory jumpers
-
-
-function zip        { Compress-Archive -Path $args[0] -DestinationPath $args[1] }
-function unzip      { Expand-Archive -LiteralPath $args[0] -DestinationPath $args[1] }
+function chainsaw   { C:\support\runners\chainsaw\chainsaw.exe $args[0] }
--- ps_functions/github-management.ps1
+++ ps_functions/github-management.ps1
@@ -29,72 +29,43 @@ function gitc     { ### evtl Parameter: repo, message
 
                         $CURRENT_PATH = (Get-Location).path                    
                         Write-Host "-------------------------------------------------------------------`n###################################################################`n-------------------------------------------------------------------"   -InformationVariable LINEDELIMITERS
-                        
-                        <#      --->> Old stuff
 
-                        ### powershell_profile (System32) Commit
-                        Write-Host "Elevating for Powershell Profile commit..." -ForegroundColor Yellow
-                        start-Sleep 1
-                        Start-Process -FilePath powershell.exe -ArgumentList { ### evtl Start-job hieraus machen
-                            Write-Host "-------------------------------------------------------------------`n###################################################################`n-------------------------------------------------------------------" -InformationVariable LINEDELIMITERS
-                            $PID # Gets Process ID of current Process
-                            Clear-Host
-                            ""
-                            $LINEDELIMITERS
-                            Set-Location $PSHOME
-                            Write-Host "Powershell Profile commit (Sys32)"
-                            ""
-                            git add .\profile.ps1
-                            ""
-                            git commit -m "Profile_Updated"
-                            ""
-                            git push
-                            ""
-                            Write-Host "Profile (Sys32) comitted" -ForegroundColor Green
-                            $LINEDELIMITERS
-                            start-Sleep 2
-                            stop-process -id $PID
-                        } -verb RunAs
-                        #>
-  
                         ### powershell_stuff Repo in C:\support\code\_git-repos\cras_stuff\POSH
                         Write-Host "PS Helper functions Repo commit (powershell_stuff)" -ForegroundColor Yellow
                         Set-Location "C:\support\code\_git-repos\cras_stuff\POSH"
                         git add .
-                        ""
                         git commit -m "Auto-commit" 
-                        ""
                         git push
-                        ""
                         Write-Host "POSH Repo comitted" -ForegroundColor Green
-                        ""
+                        
                         ###  get-adinfo Repo in C:\support\code\_git-repos\cras_stuff\get-ADInfo
                         $LINEDELIMITERS
-                        ""
                         Write-Host "Get-ADInfo Repo commit" -ForegroundColor Yellow
-                        ""
                         Set-Location "C:\support\code\_git-repos\cras_stuff\get-ADInfo"
                         git add .
-                        ""
                         git commit -m "Auto-commit" 
-                        ""
                         git push
-                        ""
                         Write-Host "Get-ADInfo Repo comitted" -ForegroundColor Green
 
                         ### iit-ansible-inventory C:\support\code\_git-repos\iit_\iit-ansible-inventory
                         $LINEDELIMITERS
                         Write-Host "iit-ansible Repo commit (iit-ansible-inventory)" -ForegroundColor Yellow
-                        ""
                         Set-Location "C:\support\code\_git-repos\iit_\iit-ansible-inventory"
                         git add .
-                        ""
                         git commit -m "Auto-commit" 
-                        ""
                         git push
-                        ""
                         Write-Host "iit-asible-inventory Repo comitted" -ForegroundColor Green
-                        ""
+                        Set-Location $CURRENT_PATH
+                        Remove-Variable -Name CURRENT_PATH
+                        
+                        ### BKG-CLI-Onboarding C:\support\code\_git-repos\iit_\BKG-CLI-Onboarding
+                        $LINEDELIMITERS
+                        Write-Host "iit-ansible Repo commit (BKG-CLI-Onboarding)" -ForegroundColor Yellow
+                        Set-Location "C:\support\code\_git-repos\iit_\BKG-CLI-Onboarding"
+                        git add .
+                        git commit -m "Auto-commit" 
+                        git push
+                        Write-Host "BKG-CLI-Onboarding" -ForegroundColor Green
                         Set-Location $CURRENT_PATH
                         Remove-Variable -Name CURRENT_PATH
 }
