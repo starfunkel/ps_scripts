95ddfc5a3683b4f88617ff4f9ef8127ec510c7fb
craiit <rathnau@integrate-it.net>
2022-12-15 15:25:28 +0100

    Auto-commit

--- ps_functions/net-mgmnt/Get-Local-IP.ps1
+++ ps_functions/net-mgmnt/Get-Local-IP.ps1
@@ -1,21 +1,14 @@
-function Get-Local-IP{
-    [CmdletBinding()]
-    [Alias("ip")]
-    param(
-       [Parameter(Mandatory=$true)]
-       [String]$Server,
-        $Filter,
-        $OrganizationalUnit
-)
-    $env:externalip = ( # gets external $ internal IPs of Localhost
-    Invoke-WebRequest "ifconfig.me/ip").Content;
-    $LOCALIPADRESS = ((ipconfig | findstr [0-9].\.)[0]).Split()[-1]
-    Write-host ""
-    Write-Output "External IP:"; 
-    $env:externalip;
-    Write-host ""
-    Write-Host "Internal IP:"
-    $LOCALIPADRESS
-    Write-host ""
-
+function ip {
+    Process {
+        $env:externalip = ( # gets external $ internal IPs of Localhost
+        Invoke-WebRequest "ifconfig.me/ip").Content;
+        $LOCALIPADRESS = ((ipconfig | findstr [0-9].\.)[0]).Split()[-1]
+        Write-host ""
+        Write-Output "External IP:"; 
+        $env:externalip;
+        Write-host ""
+        Write-Host "Internal IP:"
+        $LOCALIPADRESS
+        Write-host ""
+    }
  }
