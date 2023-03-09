4edc5a7ac4ec28e5bdd36c6aaf455e5ace886944
craiit <rathnau@integrate-it.net>
2023-03-09 12:24:50 +0100

    Auto-commit

--- ps_functions/base64-helper.ps1
+++ ps_functions/base64-helper.ps1
@@ -1,4 +1,2 @@
-set-executionpolicy 
-
 function to-base64    {[Convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes('[0]'))}
 function from-base64  {[Text.Encoding]::Utf8.GetString([Convert]::FromBase64String('[0]'))}
