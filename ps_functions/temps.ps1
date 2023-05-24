0c13961a9d0483843569c193247e4454b8c841e0
starfunkel <42552890+starfunkel@users.noreply.github.com>
2023-05-24 23:52:43 +0200

    Update temps.ps1

--- ps_functions/temps.ps1
+++ ps_functions/temps.ps1
@@ -1,4 +1,4 @@
 ### temps
 
 ### Reimport get-adinfo.psm1 for development
-function reimp      { import-module "C:\support\code\_git-repos\cras_stuff\get-ADInfo\get-adinfo.psm1" -force }
+function reimp      { import-module "C:\support\code\_git-repos\cras_stuff\get-ADInfo\get-adinfo.ps1" -force }
