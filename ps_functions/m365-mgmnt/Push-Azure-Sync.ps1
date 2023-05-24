3c505e636c14cb25ee6006f02cbd6abdcba6eb7f
starfunkel <42552890+starfunkel@users.noreply.github.com>
2023-05-25 00:00:10 +0200

    Update Push-Azure-Sync.ps1

--- ps_functions/m365-mgmnt/Push-Azure-Sync.ps1
+++ ps_functions/m365-mgmnt/Push-Azure-Sync.ps1
@@ -1,16 +1,15 @@
-# Exchange Ad-hoc functions
-# function push-azure-sync {
-    
-#    [CmdletBinding()]
-#     param (
-#        #[Parameter(Mandatory=$true)]
-#        #[Alias("pas")]
-#    )
-#     Connect-AzureAD
-#     Import-Module ADSync
-#     Get-ADSyncScheduler
-#     Start-ADSyncSyncCycle -PolicyType Delta
-#     start-Sleep -seconds 30
-#     Start-ADSyncSyncCycle -PolicyType Initial
-#     Disconnect-AzureAD
-#  }
+function push-azure-sync {
+
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$true)]
+    [Alias("pas")]
+)
+Connect-AzureAD
+Import-Module ADSync
+Get-ADSyncScheduler
+Start-ADSyncSyncCycle -PolicyType Delta
+start-Sleep -seconds 30
+Start-ADSyncSyncCycle -PolicyType Initial
+Disconnect-AzureAD
+}
