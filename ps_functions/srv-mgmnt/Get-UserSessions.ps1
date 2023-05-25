43f7b64cab295f6c3860dbe78200edb81d1ab9bc
starfunkel <cra.memb@posteo.net>
2023-05-25 21:38:42 +0200

    work

--- ps_functions/srv-mgmnt/Get-UserSessions.ps1
+++ ps_functions/srv-mgmnt/Get-UserSessions.ps1
@@ -1,14 +1,14 @@
 # Query user session information using Get-WmiObject
-$sessionInfo = Get-WmiObject -Class Win32_LogonSession -Filter "LogonType='Interactive' OR LogonType='RemoteInteractive'"
+# $sessionInfo = Get-WmiObject -Class Win32_LogonSession -Filter "LogonType='Interactive' OR LogonType='RemoteInteractive'"
 
-# Display session information
-foreach ($session in $sessionInfo) {
-    $user = $session.GetRelated("Win32_Account") | Select-Object -ExpandProperty Name
-    $logonTime = [Management.ManagementDateTimeConverter]::ToDateTime($session.StartTime)
-    $sessionType = $session.LogonType
+# # Display session information
+# foreach ($session in $sessionInfo) {
+#     $user = $session.GetRelated("Win32_Account") | Select-Object -ExpandProperty Name
+#     $logonTime = [Management.ManagementDateTimeConverter]::ToDateTime($session.StartTime)
+#     $sessionType = $session.LogonType
 
-    Write-Output "User: $user"
-    Write-Output "Logon Time: $logonTime"
-    Write-Output "Session Type: $sessionType"
-    Write-Output "-------------------------"
-}
+#     Write-Output "User: $user"
+#     Write-Output "Logon Time: $logonTime"
+#     Write-Output "Session Type: $sessionType"
+#     Write-Output "-------------------------"
+# }
