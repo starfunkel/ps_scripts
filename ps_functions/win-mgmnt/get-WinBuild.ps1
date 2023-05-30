b9933f2168a94dfe336fb4ed2e9660e4c8dcc3cc
starfunkel <cra.memb@posteo.net>
2023-05-30 15:37:03 +0200

    module installs added - get-winbuild.ps1 added

---
+++ ps_functions/win-mgmnt/get-WinBuild.ps1
@@ -0,0 +1,2 @@
+function Get-WinBuild {Get-ItemProperty -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion" | 
+Select-Object ProductName, ReleaseId, InstallationType, CurrentMajorVersionNumber,CurrentMinorVersionNumber,CurrentBuild}
