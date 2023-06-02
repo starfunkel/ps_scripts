752f8c34a621aef037a5f788ce1dc6138610364e
starfunkel <cra.memb@posteo.net>
2023-06-02 12:51:52 +0200

    ..

--- ps-scripts/log-mgmnt/Get-last_4625.ps1
+++ ps-scripts/log-mgmnt/Get-last_4625.ps1
@@ -1,18 +1,18 @@
+# If logpath is present in %SYSTEMROOT%\System32\winevt\logs
 function get-4625 {
     $filter4625='<QueryList>
-    <Query Id="0" Path="Security">
-    <Select Path="Security">*[System[(EventID=4625) and TimeCreated[timediff(@SystemTime) &lt;= 86400000]]]</Select>
+    <Query Id="0" Path="file://C:\support\playground\log_audit\logs\BKG\BKG-WTS-01_Security.evtx">
+      <Select Path="file://C:\support\playground\log_audit\logs\BKG\BKG-WTS-01_Security.evtx">*[System[(EventID=4625)]]</Select>
     </Query>
-    <QueryList>'
-    $LogonEvents = Get-WinEvent -FilterXml $filter4625
+  </QueryList>'
+    $LogonEvents = Get-WinEvent -FilterXml $filter4625 
     $LogonEvents | sort -Property TimeCreated 
-
 }
 
 $filter='<QueryList>
- <Query Id="0" Path="Security">
- <Select Path="Security">*[System[(EventID=4625) and TimeCreated[timediff(@SystemTime) &lt;= 86400000]]]</Select>
- </Query>
- </QueryList>'
- $LogonEvents = Get-WinEvent -FilterXml $filter
- $LogonEvents | sort -Property TimeCreated #| Select-Object -First 1
+<Query Id="0" Path="Security">
+<Select Path="Security">*[System[(EventID=4625) and TimeCreated[timediff(@SystemTime) &lt;= 86400000]]]</Select>
+</Query>
+</QueryList>'
+$LogonEvents = Get-WinEvent -FilterXml $filter
+$LogonEvents | sort -Property TimeCreated #| Select-Object -First 1
--- ps_functions/temps.ps1
+++ ps_functions/temps.ps1
@@ -2,3 +2,4 @@
 
 ### Reimport get-adinfo.psm1 for development
 function reimp      { import-module "C:\support\code\_git-repos\cras_stuff\get-ADInfo\get-adinfo.ps1" -force }
+n
