71f6d8227d04b2978f370ac359246a2be9fd30d8
starfunkel <cra.memb@posteo.net>
2023-06-20 13:47:35 +0200

    refined from and to base64 helper

--- ps-scripts/ad-mgmnt/Get-SPNReport.ps1
+++ ps-scripts/ad-mgmnt/Get-SPNReport.ps1
@@ -1,57 +1,60 @@
-function get-spn {
-
-	param (
-		[string]$computerName,
-		[string]$userName,
-		[switch]$allComputers,
-		[switch]$allUsernames,
-		[switch]$exportToCsv,
-		[switch]$outputAsList
+function Get-SPN {
+    param (
+        [string]$computerName,
+        [string]$userName,
+        [switch]$allComputers,
+        [switch]$allUsernames,
+        [switch]$exportToCsv,
+        [switch]$outputAsList,
+        # [string]$csvPath = "C:\admin\ad_spns.csv"
+		[string]$csvPath = "C:\support\code\git-repos\starfunkel\powershell_stuff\ps-scripts\ad-mgmnt"
 	)
-	
-	$rootDSE = New-Object System.DirectoryServices.DirectoryEntry("LDAP://RootDSE")
-	$defaultNamingContext = $rootDSE.defaultNamingContext
-	
-	$searcher = New-Object System.DirectoryServices.DirectorySearcher
-	$searcher.SearchRoot = New-Object System.DirectoryServices.DirectoryEntry("LDAP://$defaultNamingContext")
-	
-	if ($allComputers) {
-		$searcher.Filter = "(&(servicePrincipalName=*)(cn=*))"
-	} elseif ($allUsernames) {
-		$searcher.Filter = "(&(servicePrincipalName=*)(samAccountName=*))"
-	} else {
-		$searcher.Filter = "(&(servicePrincipalName=*)(|(cn=$computerName)(samAccountName=$userName)))"
-	}
-	
-	$searcher.PageSize = 1000
-	$searcher.PropertiesToLoad.AddRange(@("name", "servicePrincipalName"))
-	
-	$results = $searcher.FindAll()
-	
-	$output = foreach ($result in $results) {
-		$entry = $result.GetDirectoryEntry()
-		$spnValues = $entry.Properties["servicePrincipalName"] | ForEach-Object { $_ -join "`n" }
-		
-		[PSCustomObject]@{
-			'Object Name' = $entry.Properties["name"] -join ';'
-			'Service Principal Names' = $spnValues
-		}
-	}
-	
-	if ($exportToCsv) {
-		$output | Select-Object 'Object Name', 'Service Principal Names' |
-			Export-Csv -Path "C:\support\playground\spn_output.csv" -NoTypeInformation
-	} elseif ($outputAsList) {
-		$output | ForEach-Object {
-			Write-Host "Object Name: $($_.'Object Name')"
-			Write-Host "Service Principal Names:"
-			Write-Host $_.'Service Principal Names'
-			Write-Host
-		}
-	} else {
-		$output | Format-Table
-	}
-	
-	$searcher.Dispose()
-	
+
+    $rootDSE = New-Object System.DirectoryServices.DirectoryEntry("LDAP://RootDSE")
+    $defaultNamingContext = $rootDSE.defaultNamingContext
+
+    $searcher = New-Object System.DirectoryServices.DirectorySearcher
+    $searcher.SearchRoot = New-Object System.DirectoryServices.DirectoryEntry("LDAP://$defaultNamingContext")
+
+    if ($allComputers) {
+        $searcher.Filter = "(&(servicePrincipalName=*)(cn=*))"
+    }
+    elseif ($allUsernames) {
+        $searcher.Filter = "(&(servicePrincipalName=*)(samAccountName=*))"
+    }
+    else {
+        $searcher.Filter = "(&(servicePrincipalName=*)(|(cn=$computerName)(samAccountName=$userName)))"
+    }
+
+    $searcher.PageSize = 1000
+    $searcher.PropertiesToLoad.AddRange(@("name", "servicePrincipalName"))
+
+    $results = $searcher.FindAll()
+
+    $output = foreach ($result in $results) {
+        $entry = $result.GetDirectoryEntry()
+        $spnValues = $entry.Properties["servicePrincipalName"] | ForEach-Object { $_ -join "`n" }
+
+        [PSCustomObject]@{
+            'Object Name' = $entry.Properties["name"] -join ';'
+            'Service Principal Names' = $spnValues -join "`n"
+        }
+    }
+
+    if ($exportToCsv) {
+        $output | Export-Csv -Path $csvPath -NoTypeInformation -Delimiter ';'
+    }
+    elseif ($outputAsList) {
+        $output | ForEach-Object {
+            Write-Host "Object Name: $($_.'Object Name')"
+            Write-Host "Service Principal Names:"
+            Write-Host $_.'Service Principal Names'
+            Write-Host
+        }
+    }
+    else {
+        $output | Sort-Object 'Object Name' | Format-Table -Wrap
+    }
+
+    $searcher.Dispose()
 }
