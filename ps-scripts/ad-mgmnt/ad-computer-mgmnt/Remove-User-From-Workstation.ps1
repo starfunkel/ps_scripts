2fa1f965f2ba544e50f26ec84547dfd2c6dabb6e
starfunkel <42552890+starfunkel@users.noreply.github.com>
2023-07-10 12:16:19 +0200

    Update Remove-User-From-Workstation.ps1

--- ps-scripts/ad-mgmnt/ad-computer-mgmnt/Remove-User-From-Workstation.ps1
+++ ps-scripts/ad-mgmnt/ad-computer-mgmnt/Remove-User-From-Workstation.ps1
@@ -1,6 +1,9 @@
-$user="testUser"
 Function Remove-UserProfile($userName)
 {
+
+    [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+    [string]$InputString
+
     $userProfile=Get-CimInstance -Class Win32_UserProfile | Where-Object { $_.LocalPath.split('\')[-1] -eq $userName } | Remove-CimInstance
     Remove-Item -Recurse -Force ($userProfile).LocalPath
 }
