447c30d1d6a0e5bff09d5c29483502f5e7876e89
starfunkel <42552890+starfunkel@users.noreply.github.com>
2023-07-10 12:03:45 +0200

    Create Get-OS-From-Domain.ps1

---
+++ ps-scripts/ad-mgmnt/ad-computer-mgmnt/Get-OS-From-Domain.ps1
@@ -0,0 +1,4 @@
+﻿Get-ADComputer -Filter 'operatingsystem -like "*Windows 10*"' -Properties  Name,Operatingsystem, OperatingSystemVersion,IPv4Address |
+Select-Object -Property Name, OperatingSystemVersion, IPv4Address |
+Group-object OperatingSystemVersion| Select Count,Name |
+Sort-Object -Descending Name
