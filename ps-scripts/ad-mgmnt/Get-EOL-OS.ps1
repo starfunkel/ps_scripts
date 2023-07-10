9918ad9b617044e02ea4ef026adc701b9f51c3c0
starfunkel <42552890+starfunkel@users.noreply.github.com>
2023-07-10 12:02:19 +0200

    Create Get-EOL-OS.ps1

---
+++ ps-scripts/ad-mgmnt/Get-EOL-OS.ps1
@@ -0,0 +1,3 @@
+﻿Get-ADComputer -Filter 'operatingsystem -like "*Windows 10*"' -Properties  Name, OperatingSystemVersion |
+Where-Object {$_.OperatingSystemVersion -like "*1836*"} |
+Select-Object -ExpandProperty Name
