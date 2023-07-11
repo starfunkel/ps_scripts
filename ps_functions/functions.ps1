2e99bd95dfcf620b82c208f92a41e5396b65027b
starfunkel <cra.memb@posteo.net>
2023-07-11 12:12:08 +0200

    functions.ps1 ident

--- ps_functions/functions.ps1
+++ ps_functions/functions.ps1
@@ -1,39 +1,40 @@
 ### Powershell Funcions
 
 ### Compute file hashes 
-function md5        { Get-FileHash -Algorithm MD5 $args }
-function sha1       { Get-FileHash -Algorithm SHA1 $args }
-function sha256     { Get-FileHash -Algorithm SHA256 $args }
+function md5            { Get-FileHash -Algorithm MD5 $args }
+function sha1           { Get-FileHash -Algorithm SHA1 $args }
+function sha256         { Get-FileHash -Algorithm SHA256 $args }
 
 ### Bash like commands
-function pwd        { Get-Location }
-function ll         { Get-Childitem | Sort-Object -Property LastWriteTime }
-function help       { get-help $args[0] | out-host -paging }
-function man        { get-help $args[0] -Full | out-host -paging }
-function cat        { get-content $args[0] }
-function less       { get-content $args[0] | out-host -paging }
-function find       { Get-Childitem -Filter $args[0] -Recurse -File | out-host -paging }
-function sudo       { Start-Process -verb RunAs wt }
-function su         { Start-Process -FilePath powershell.exe -ArgumentList[0] }
-function history    { get-history }
-function touch      { New-Item $args[0] }
-function zip        { Compress-Archive -Path $args[0] -DestinationPath $args[1] }
-function unzip      { Expand-Archive -LiteralPath $args[0] -DestinationPath $args[1] }
+function pwd            { Get-Location }
+function ll             { Get-Childitem | Sort-Object -Property LastWriteTime }
+function help           { get-help $args[0] | out-host -paging }
+function man            { get-help $args[0] -Full | out-host -paging }
+function cat            { get-content $args[0] }
+function less           { get-content $args[0] | out-host -paging }
+function find           { Get-Childitem -Filter $args[0] -Recurse -File | out-host -paging }
+function sudo           { Start-Process -verb RunAs wt }
+function su             { Start-Process -FilePath powershell.exe -ArgumentList[0] }
+function history        { get-history }
+function touch          { New-Item $args[0] }
+function zip            { Compress-Archive -Path $args[0] -DestinationPath $args[1] }
+function unzip          { Expand-Archive -LiteralPath $args[0] -DestinationPath $args[1] }
 
-function uname      { $PROPERTIES = 'Caption', 'CSName', 'Version', 'BuildType', 'OSArchitecture'; Get-CimInstance Win32_OperatingSystem | 
-                        Select-Object $PROPERTIES |
-                        Format-Table -AutoSize 
-                    }
+function uname          { $PROPERTIES = 'Caption', 'CSName', 'Version', 'BuildType', 'OSArchitecture'; Get-CimInstance Win32_OperatingSystem | 
+                            Select-Object $PROPERTIES |
+                            Format-Table -AutoSize 
+                        }
 
 ### Custom functions
-function pong       { Test-Connection $args[0] | Format-Table -Autosize }
-function imp        { Import-Module -Name $args[0] }
+function pong           { Test-Connection $args[0] | Format-Table -Autosize }
+function imp            { Import-Module -Name $args[0] }
 
 ### Windows Shortcuts
 
-function credman    { rundll32 keymgr.dll, KRShowKeyMgr }
-function mycreds    {﻿Get-ChildItem -path cert:\LocalMachine\My}
+function credman        { rundll32 keymgr.dll, KRShowKeyMgr }
+function userprofiles   { rundll32 sysdm.cpl,EditUserProfiles }
+function mycreds        { Get-ChildItem -path cert:\LocalMachine\My }
 
-function get-random  {﻿Add-Type -AssemblyName System.web
-                      [System.Web.Security.Membership]::GeneratePassword(32,0)
-                      }
+function get-random     { Add-Type -AssemblyName System.web
+                        [System.Web.Security.Membership]::GeneratePassword(32,0)
+                        }
