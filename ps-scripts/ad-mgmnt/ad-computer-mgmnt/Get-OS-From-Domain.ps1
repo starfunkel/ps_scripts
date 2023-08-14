d9751a7003da99ea7d27f951e8edb5504dc55178
starfunkel <cra.memb@posteo.net>
2023-08-14 09:47:34 +0200

    ...

--- ps-scripts/ad-mgmnt/ad-computer-mgmnt/Get-OS-From-Domain.ps1
+++ ps-scripts/ad-mgmnt/ad-computer-mgmnt/Get-OS-From-Domain.ps1
@@ -1,4 +1,6 @@
-﻿Get-ADComputer -Filter 'operatingsystem -like "*Windows Server*"' -Properties  Name,Operatingsystem, OperatingSystemVersion,IPv4Address |
-Select-Object -Property Name, OperatingSystemVersion, IPv4Address #|
-# Group-object OperatingSystemVersion| Select-Object Count,Name |
-Sort-Object -Descending OperatingSystemVersion
+﻿Get-ADComputer -Filter {OperatingSystem -like "Windows Server*" -and Enabled -eq $true} -Property OperatingSystemVersion |
+    Select-Object -Property Name, @{
+        Name = "BuildNumber"
+        Expression = { $_.OperatingSystemVersion -replace "^10\.0\.(\d+)\.\d+$", '$1' }
+    } |
+    Sort-Object -Property BuildNumber
