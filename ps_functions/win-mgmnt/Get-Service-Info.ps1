098be0b5ceb7e7f8e7dbee9da4bb1da5613995bf
starfunkel <cra.memb@posteo.net>
2023-08-15 15:52:58 +0200

    get-service script mod

--- ps_functions/win-mgmnt/Get-Service-Info.ps1
+++ ps_functions/win-mgmnt/Get-Service-Info.ps1
@@ -1,6 +1,6 @@
 function Get-Service-Info {
     Get-CimInstance Win32_Service |
-    Select-Object Name,DisplayName,Status,StartName,PathName |
-    Sort-Object Name |
+    Select-Object Name,DisplayName,StartMode,State,Status,StartName,PathName |
+    Sort-Object StartName |
     Format-Table
 }
