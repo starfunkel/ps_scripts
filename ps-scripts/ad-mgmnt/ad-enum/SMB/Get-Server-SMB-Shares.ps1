cc2984f44bcd5ef78c947136f255d4210f1f6616
starfunkel <cra.memb@posteo.net>
2023-10-17 12:31:19 +0200

    Sorted spn and smb enum ps1s

---
+++ ps-scripts/ad-mgmnt/ad-enum/SMB/Get-Server-SMB-Shares.ps1
@@ -0,0 +1,19 @@
+# Retrieve Server Names 
+$server = Get-ADComputer -Filter 'operatingsystem -like "*server*"' | 
+Select-Object -ExpandProperty Name
+
+# Collection Point 
+$result = @()
+ 
+# Show all Shares of all Servers
+foreach ($s in $server) {
+    $shares = Invoke-Command -ComputerName $s -ScriptBlock {Get-SmbShare |
+     Select-Object PSComputerName,Name,Path} 
+    $result += New-Object PSObject -Property ([ordered]@{ 
+        ServerName  = $s
+        Shares      = $shares.Name -join "`n"
+        Path        = $shares.Path -join "`n"
+    })
+}
+Write-Output $result |
+Format-Table -AutoSize -Wrap
---
+++ ps-scripts/ad-mgmnt/ad-enum/SMB/Get-active-SMB-Session.ps1
@@ -0,0 +1,32 @@
+<#
+.SYNOPSIS
+Helper Script to identify all active smb sessions and their type (dialect) of a given server
+arreay
+.DESCRIPTION 
+
+.EXAMPLE
+
+.LINK
+
+
+.NOTES
+Written by: Christian Rathnau
+
+Website:	https://www.integrate-it.net/
+#>
+
+$Server = "ii-lap-60"
+
+$Connections = Get-SmBConnection -Server $Server
+
+if ($Connections) {
+    foreach ($Connection in $Connections) {
+        Write-Host "Connection ID: $($Connection.Id)"
+        Write-Host "Client: $($Connection.ClientComputerName)"
+        Write-Host "Dialect used $($Connection.Dialect)"
+        Write-Host "SMB Version: $($Connection.SmbVerion)"
+        Write-Host
+    }
+} else {
+    Write-Host "No active SMB connections found"
+}
