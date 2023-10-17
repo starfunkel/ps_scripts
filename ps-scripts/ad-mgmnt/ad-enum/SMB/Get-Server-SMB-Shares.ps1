fe0aa849c357f41bf7acc7bce00654a8f34367bb
starfunkel <cra.memb@posteo.net>
2023-10-17 14:31:17 +0200

    ff

--- ps-scripts/ad-mgmnt/ad-enum/SMB/Get-Server-SMB-Shares.ps1
+++ ps-scripts/ad-mgmnt/ad-enum/SMB/Get-Server-SMB-Shares.ps1
@@ -4,16 +4,14 @@ Select-Object -ExpandProperty Name
 
 # Collection Point 
 $result = @()
- 
+
 # Show all Shares of all Servers
 foreach ($s in $server) {
-    $shares = Invoke-Command -ComputerName $s -ScriptBlock {Get-SmbShare |
-     Select-Object PSComputerName,Name,Path} 
+    $shares = Invoke-Command -ComputerName $s -ScriptBlock {Get-SmbShare | Select-Object PSComputerName,Name,Path} 
     $result += New-Object PSObject -Property ([ordered]@{ 
         ServerName  = $s
         Shares      = $shares.Name -join "`n"
         Path        = $shares.Path -join "`n"
     })
 }
-Write-Output $result |
-Format-Table -AutoSize -Wrap
+Write-Output $result | Format-Table -AutoSize -Wrap
