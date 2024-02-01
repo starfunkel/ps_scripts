59f9915f1aed36832dd874f83180f0c3e074a926
starfunkel <cra.memb@posteo.net>
2024-02-01 17:47:14 +0100

    chenged upn seach and replace with email scripts

--- ps-scripts/ad-mgmnt/ad-user-mgmnt/Change-User-UPN-OU-wide.ps1
+++ ps-scripts/ad-mgmnt/ad-user-mgmnt/Change-User-UPN-OU-wide.ps1
@@ -1,30 +1,19 @@
-# Change UPN on Users in OU and sub OUs
-# Prompt for Organizational Unit (OU)
-$ou = Read-Host "Enter the Organizational Unit (OU)"
+# OU as searchbase
+$ouDN = ""
 
-# Prompt for User Principal Name (UPN) to find
-$currentUpn = Read-Host "Enter the current User Principal Name (UPN) to find"
+# populate $users with all AD-users in search base
+$users = Get-ADUser -Filter * -SearchBase $ouDN
 
-# Search for users with the specified UPN
-$users = Get-ADUser -Filter {UserPrincipalName -eq $currentUpn} -SearchBase $ou -SearchScope Subtree
+foreach ($user in $users) {
+    # Error handling - discard ad-user without email adress
+    if ($user.EmailAddress) {
+        $newUPN = $user.EmailAddress
 
-# Display found UPNs
-Write-Host "Found UPNs:"
-$users | ForEach-Object { $_.UserPrincipalName }
+        # Error handling - skip if already $newupn = $user.EmailAddress
+        if ($user.UserPrincipalName -ne $newUPN) {
 
-# Ask for confirmation to proceed with the change
-$confirmation = Read-Host "Do you want to change the UPN for the above users? (Y/N)"
-if ($confirmation -eq 'Y') {
-    # Prompt for the new UPN
-    $newUpn = Read-Host "Enter the new User Principal Name (UPN)"
-
-    # Update UPN for all users
-    foreach ($user in $users) {
-        Set-ADUser -Identity $user -UserPrincipalName $newUpn
-        Write-Host "Updated UPN for $($user.SamAccountName)"
+            Set-ADUser -Identity $user -UserPrincipalName $newUPN
+            Write-Host "Updated UPN for $($user.SamAccountName) to $newUPN" -ForegroundColor Green
+        }
     }
-
-    Write-Host "UPN update completed."
-} else {
-    Write-Host "UPN update aborted."
 }
