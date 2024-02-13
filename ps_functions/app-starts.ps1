0097245f94f8102c9b06cfcf531388bf3cead54e
starfunkel <cra.memb@posteo.net>
2024-02-13 09:32:29 +0100

    debugging tool chain added

--- ps_functions/app-starts.ps1
+++ ps_functions/app-starts.ps1
@@ -6,8 +6,11 @@ function mmc        { & "C:\Windows\SysWOW64\mmc.exe" }
 function cmmc       { & "C:\support\code\configs\cmmc.msc" }
 function winscp     { & "C:\support\tools\Portable_APPS\winscp\WinSCP.exe"} 
 function baobab     { & "C:\support\tools\Portable_APPS\WinDirStatPortable\App\WinDirStat\windirstat.exe" }
+
+### coding
 function x64dgb     { & "C:\support\tools\x64dgb\release\x64\x64dbg.exe"}
-function wifi       { & "C:\support\tools\WifiInfoView.exe"}
+function hxd        { & "C:\Program Files\HxD\HxD.exe"}
+function ghidra     { & "C:\support\tools\dig_deeper\debug\ghidra\ghidraRun.bat"}
 
 ### Firefox
 function ffd        { & "$env:ProgramFiles\Mozilla Firefox\firefox.exe" -p default }
@@ -25,10 +28,11 @@ function tux        { Get-Cowsay -cowfile tux "$args" }
 function winfetch   { "C:\support\code\_git-repos\foreign\winfetch\winfetch.ps1" }
 
 ### vrtualbox
-function vboxmanage { & "$env:ProgramFiles\Oracle\VirtualBox\VBoxManage.exe" $args[0] $args[1] $args[2] }
-function start-lab  { & "$env:ProgramFiles\Oracle\VirtualBox\VBoxManage.exe" startvm "VM-LX-SRV-DEBIAN-11" --type headless}
+# function vboxmanage { & "$env:ProgramFiles\Oracle\VirtualBox\VBoxManage.exe" $args[0] $args[1] $args[2] }
+# function start-lab  { & "$env:ProgramFiles\Oracle\VirtualBox\VBoxManage.exe" startvm "VM-LX-SRV-DEBIAN-11" --type headless}
 
+### network
 function nm         { & "C:\Program Files\Microsoft Network Monitor 3\netmon.exe"}
 function nmwifi     { & "C:\Program Files\Microsoft Network Monitor 3\nmwifi.exe"}
 function vim	    { & "C:\Program Files\Neovim\bin\nvim.exe" $args[0] }
-
+function wifi       { & "C:\support\tools\network\WifiInfoView.exe"}
