3bfeff4203f6f527e2a79e6e7482415601ffe1f1
starfunkel <cra.memb@posteo.net>
2024-04-09 10:32:34 +0200

    forget ist jetzt fg

--- ps_functions/functions.ps1
+++ ps_functions/functions.ps1
@@ -17,7 +17,7 @@ function find           { Get-Childitem -Filter $args[0] -Recurse -File | out-ho
 function sudo           { Start-Process -verb RunAs $args[0] }
 function hist           { Get-Content (Get-PSReadlineOption).HistorySavePath | more }
 
-function forget         {Remove-Item -Path (Get-PSReadlineOption).HistorySavePath -Force
+function fg              {Remove-Item -Path (Get-PSReadlineOption).HistorySavePath -Force
                             Clear-Host
                             pwsh.exe
                         }
