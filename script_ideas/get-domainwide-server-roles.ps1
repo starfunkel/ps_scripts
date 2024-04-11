ea4ba743bc663a96bb648b0bd313578c18b671c2
starfunkel <cra.memb@posteo.net>
2024-04-11 17:12:36 +0200

    more sxript ideas

---
+++ script_ideas/get-domainwide-server-roles.ps1
@@ -0,0 +1,36 @@
+# Retrieving all Servers by Name
+$ErrorActionPreference = "SilentlyContinue"
+$servers = (Get-ADComputer -Properties operatingsystem `
+-Filter 'operatingsystem -like "*server*" -and enabled -eq "true"').Name
+ 
+Collection Point
+$result = @()
+ 
+# Ping all Server and if successful run Get-WindowsFeature 
+foreach ($item in $servers) {
+        $Test = Test-Connection $item -Count 1
+Providing PowerShell 7 and 5.1 compatibility in terms of return code 
+    If ($Test.Status -eq 'Success' -or $Test.StatusCode -eq '0') {
+        $roles = Get-WindowsFeature -ComputerName $item |
+        Where-Object InstallState -EQ 'Installed'
+        Write into collection point
+        $result += New-Object -TypeName PSObject -Property ([ordered]@{
+                    'Server' = $item
+                    'Roles'  = $roles.Name -join "`r`n" ### Escape Character CarriageReturn (`r) + NewLine (`n)
+        }
+        )
+    }
+}
+
+# Generating Outputs
+Collecting Point: Console Output
+Write-Output $result | Format-Table -Wrap
+ 
+# Text File Output
+$date = Get-Date -Format MM.dd.yyyy
+$result | Format-Table -Wrap |
+Out-File "$HOME\ServerRoles$date.txt"
+Write-Warning "Output file ServerRoles$date.txt generated in $HOME ... Opening file ..."
+ 
+# Opening File
+Start-Process $HOME\"ServerRoles$date.txt"
