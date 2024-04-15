0679473a94b56914deb09b7629d4f1454fbd34ff
starfunkel <cra.memb@posteo.net>
2024-04-15 16:36:38 +0200

    added sec. string ref

---
+++ script_ideas/powershell_concepts_as functions_or help file/SecureString.ps1
@@ -0,0 +1,6 @@
+$password = ConvertTo-SecureString 'P@ssw0rd' -AsPlainText -Force
+
+$Ptr = [System.Runtime.InteropServices.Marshal]::SecureStringToCoTaskMemUnicode($password)
+$result = [System.Runtime.InteropServices.Marshal]::PtrToStringUni($Ptr)
+[System.Runtime.InteropServices.Marshal]::ZeroFreeCoTaskMemUnicode($Ptr)
+$result 
