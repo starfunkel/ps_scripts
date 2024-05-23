72125cdd188bb26c185dddfc7948018b21222d53
starfunkel <cra.memb@posteo.net>
2024-05-23 11:01:55 +0200

    mod sdcount clearing script

--- ps_scripts/ad_mgmnt/ad_user_mgmnt/Reset-AD-AdminCount.ps1
+++ ps_scripts/ad_mgmnt/ad_user_mgmnt/Reset-AD-AdminCount.ps1
@@ -1,4 +1,5 @@
 $users = Get-ADUser -Filter 'adminCount -eq 1' -Properties adminCount
 foreach ($user in $users) {
-        $user | Set-ADObject -Clear adminCount -whatif
+        Set-Aduser -Identity $user -Replace @{adminCount=0} -whatif
 }
+
