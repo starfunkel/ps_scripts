69b4024b0c83980988094ae7906a0310927e9760
starfunkel <cra.memb@posteo.net>
2024-07-08 15:13:02 +0200

    added chainsaw dir helper

---
+++ env/def_chainsaw_dir.ps1
@@ -0,0 +1,3 @@
+$sigma = "C:\support\code\git-repos\starfunkel\chainsaw\sigma"
+$mapping = "C:\support\code\git-repos\starfunkel\chainsaw\mappings"
+$rules = "C:\support\code\git-repos\starfunkel\chainsaw\rules"
