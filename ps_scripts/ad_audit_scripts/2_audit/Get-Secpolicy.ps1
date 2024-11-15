e2723a40a155d376931e15da81ccdb876226dfd5
starfunkel <cra.memb@posteo.net>
2024-11-15 13:14:28 +0100

    Mind the language todo

--- ps_scripts/ad_audit_scripts/2_audit/Get-Secpolicy.ps1
+++ ps_scripts/ad_audit_scripts/2_audit/Get-Secpolicy.ps1
@@ -1,3 +1,5 @@
+## Translate with if to englisch lang
+
 auditpol.exe /get /category:* /r  | 
     ConvertFrom-Csv |
         Format-Table Richtlinienziel,Unterkategorie,Aufnahmeeinstellung,Ausschlusseinstellung
