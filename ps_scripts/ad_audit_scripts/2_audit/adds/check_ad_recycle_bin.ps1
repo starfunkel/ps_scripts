014c4d87fc80cb320cf63c288247af3b57a0d363
starfunkel <cra.memb@posteo.net>
2024-11-21 09:41:12 +0100

    added testlogon script to produce failed logons

--- ps_scripts/ad_audit_scripts/2_audit/adds/check_ad_recycle_bin.ps1
+++
@@ -1,9 +0,0 @@
-Get-ADObject -Filter 'isDeleted -eq $True -and -not (isRecycled -eq $True) -and name -ne "Deleted Objects"' -IncludeDeletedObjects 
-
-# restore
-
-<#
-$object=""
-get-adobject -filter 'name -like "Santa*"' -IncludeDeletedObjects |
- Restore-ADObject –TargetPath "OU,CN..." -passthru
- #>
