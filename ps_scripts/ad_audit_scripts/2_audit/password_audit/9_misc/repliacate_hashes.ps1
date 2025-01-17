aba8b67df135ad3c74cdda758e287e5906f0f613
starfunkel <cra.memb@posteo.net>
2025-01-17 11:23:05 +0100

    ...

--- ps_scripts/ad_audit_scripts/2_audit/password_audit/9_misc/repliacate_hashes.ps1
+++ ps_scripts/ad_audit_scripts/2_audit/password_audit/9_misc/repliacate_hashes.ps1
@@ -2,9 +2,6 @@
 #
 ###### UNTESTED
 #
-
-$domain = ""
-$username = ""
 $srv = ""
 
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted
@@ -14,7 +11,7 @@ Install-Module -Name DSInternals
 
 $creds = Get-Credential
 
-Get-ADReplAccount -All -Credential $creds -Server $server -Protocol TCP | Format-Custom -View HashcatNT | Out-File hashes.txt -Encoding utf8
+Get-ADReplAccount -All -Credential $creds -Server $srv -Protocol TCP | Format-Custom -View HashcatNT | Out-File hashes.txt -Encoding utf8
 
 # Replicate a single account
 # Get-ADReplAccount -SamAccountName $username -Domain $domain -Server $server -Credential $creds -Protocol TCP
