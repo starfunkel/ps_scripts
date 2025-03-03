81adc268f0663260bc51c27def001b440f282606
starfunkel <cra.memb@posteo.net>
2025-03-03 16:33:47 +0100

    desc & build fucntion for ms-DS-MachineAccountQuota attribute

--- ps_scripts/ad_audit_scripts/3_fix_it/set-ms-ds-machineaccountqouta.ps1
+++ ps_scripts/ad_audit_scripts/3_fix_it/set-ms-ds-machineaccountqouta.ps1
@@ -1,3 +1,13 @@
-# Get-Adobject (Get-ADDomain) -Properties ms-DS-MachineAccountQuota
+<#
+.SYNOPSIS
+    Set ms-DS-MachineAccountQuota from 10 to 0
 
+.DESCRIPTION
+    Sets the default number of allowed computer objects which can be added to a
+    domain from 10 to 0. This ensures that only domain admins by default can 
+    add computer objects 
+
+#>
+Function Set-ms-DS-MachineAccountQuota {
 Set-Adobject ((Get-ADDomain).name) -replace @(ms-DS-MachineAccountQuota = "0")
+}
