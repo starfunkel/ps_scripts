97d08af4b423fc99a95f7bdb345d39e6e2ad0870
starfunkel <cra.memb@posteo.net>
2025-03-03 16:30:29 +0100

    rewrite & desc of set adminCount

--- ps_scripts/ad_audit_scripts/3_fix_it/set-adminCount.ps1
+++ ps_scripts/ad_audit_scripts/3_fix_it/set-adminCount.ps1
@@ -1,2 +1,30 @@
-Get-ADUser -Filter 'adminCount -eq 1' -Properties adminCount | select name
-#Set-ADUser -Identity $SAAMAccountname administrator -Replace @{adminCount=0}
+<#
+.SYNOPSIS
+    Resets AdminCount flag on AD-User's which have this flag. 
+#>
+
+function Reset-AdminCount {
+    param (
+        [CmdletBinding()]
+        [switch]$ConfirmDeletion
+    )
+
+    $adminUsers = Get-ADUser -Filter 'adminCount -eq 1' -Properties adminCount | Select-Object Name, SamAccountName
+    
+    if ($adminUsers) {
+        Write-Host "AD-Users with adminCount set to 1:" -ForegroundColor Yellow
+        $adminUsers | Format-Table -AutoSize
+
+        $response = Read-Host "Reset flag (Y/N)?"
+        if ($ConfirmDeletion -or $response -eq 'Y') {
+            foreach ($user in $adminUsers) {
+                Set-ADUser -Identity $user.SamAccountName -Replace @{adminCount=0} -WhatIf
+                Write-Host "Reset adminCount for: $($user.Name)" -ForegroundColor Green
+            }
+        } else {
+            Write-Host "Aborting" -ForegroundColor Cyan
+        }
+    } else {
+        Write-Host "No users found with adminCount set to 1." -ForegroundColor Cyan
+    }
+}
