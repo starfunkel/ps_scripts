9e8255248678cc2982734acbdf685ed5eafae6e3
starfunkel <cra.memb@posteo.net>
2025-03-03 14:33:01 +0100

    add desc & made function

--- ps_scripts/ad_audit_scripts/2_audit/ous/Get-empty-OUs.ps1
+++ ps_scripts/ad_audit_scripts/2_audit/ous/Get-empty-OUs.ps1
@@ -1,3 +1,8 @@
+<#
+.SYNOPSIS
+    Get Empty OUs
+#>
+fucntion Get-Empty-OUs {
 Get-ADOrganizationalUnit -filter * -Properties Description -PipelineVariable pv |
 Select-Object DistinguishedName,Name,Description,
 @{Name="Children"; Expression = {
@@ -5,3 +10,4 @@ Get-ADObject -filter * -SearchBase $pv.distinguishedname |
 Where-Object { $_.objectclass -ne "organizationalunit"} |
 Measure-Object | Select-Object -ExpandProperty Count }} |
 Where-Object {$_.children -eq 0}
+}
