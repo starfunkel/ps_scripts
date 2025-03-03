82e64a632a8c9ac20a8e70629268bf3e35468299
starfunkel <cra.memb@posteo.net>
2025-03-03 13:40:54 +0100

    add desc & mv

--- ps_scripts/ad_audit_scripts/2_audit/ad-users/Get-All-Disabled-users.ps1
+++
@@ -1,3 +0,0 @@
-Get-ADUser -Filter {Enabled -eq $false} -Properties samaccountname, Name, lastlogondate, LastBadpasswordAttempt, PasswordLastSet |
-Sort-Object LastLogonDate |
-Format-Table samaccountname, Name, lastlogondate, LastBadpasswordAttempt, PasswordLastSet
