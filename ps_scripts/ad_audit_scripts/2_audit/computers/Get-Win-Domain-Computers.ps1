4320f4566ee18f6254beb57e7d3876204159788e
starfunkel <cra.memb@posteo.net>
2025-03-03 16:50:01 +0100

    rewrite get ad computer objects by OS

---
+++ ps_scripts/ad_audit_scripts/2_audit/computers/Get-Win-Domain-Computers.ps1
@@ -0,0 +1,19 @@
+<#
+.SYNOPSIS
+    This script gathers all AD-Computer accounts which are not Windows Server OS based
+    in the current domain with OS, OS version, and IPv4 Address.
+#>
+function FunctionName {
+    Write-Host -ForegroundColor Green "Windows Computer Objects $env:userdnsdomain"
+    $computers = Get-ADComputer -Filter {OperatingSystem -notlike "*server*" -and OperatingSystem -notlike "unknown"} `
+        -Properties Name, OperatingSystem, OperatingSystemVersion, IPv4Address 
+    $ccount = $computers | Measure-Object | Select-Object -ExpandProperty Count
+    $computers | 
+    Sort-Object OperatingSystem | 
+    Format-Table Name, OperatingSystem, OperatingSystemVersion, IPv4Address
+        Write-Host -ForegroundColor Yellow "Total: $ccount"
+}
+
+
+
+
---
+++ ps_scripts/ad_audit_scripts/2_audit/computers/Get-Win-Domain-Server.ps1
@@ -0,0 +1,16 @@
+<#
+.SYNOPSIS
+    This script gathers all Windows Servers of the current domain with OS, OS version, and IPv4 Address 
+
+#>
+Function Get-Win-Domain-Server {
+    Write-Host -ForegroundColor Green "Windows Server $env:userdnsdomain" 
+    $server=Get-ADComputer -Filter {operatingsystem -like "*server*"} -Properties Name,Operatingsystem,OperatingSystemVersion,IPv4Address 
+    $scount=$server | 
+    Measure-Object | 
+    Select-Object -ExpandProperty count
+    Write-Output $server |
+    Sort-Object Operatingsystem |
+    Format-Table Name,Operatingsystem,OperatingSystemVersion,IPv4Address
+    Write-Host "Total: "$scount"" -ForegroundColor Yellow
+}
