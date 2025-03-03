56b8c36c7d26f838f4557ccce4d97b9c3bb79b54
starfunkel <cra.memb@posteo.net>
2025-03-03 13:32:07 +0100

    mod and move ad server audit script

--- ps_scripts/ad_audit_scripts/2_audit/servers/Get-Win-Domain-Server.ps1
+++ ps_scripts/ad_audit_scripts/2_audit/servers/Get-Win-Domain-Server.ps1
@@ -1,3 +1,14 @@
+<#
+.SYNOPSIS
+    This script gathers all Windows Servers of the current domain with OS, OS version, and IPv4 Address 
+
+.EXAMPLE
+    Example usage of the script.
+
+.NOTES
+    Additional notes, author, version, etc.
+
+#>
 Write-Host -ForegroundColor Green "Windows Server $env:userdnsdomain" 
 $server=Get-ADComputer -Filter {operatingsystem -like "*server*"} -Properties Name,Operatingsystem,OperatingSystemVersion,IPv4Address 
 $scount=$server | 
