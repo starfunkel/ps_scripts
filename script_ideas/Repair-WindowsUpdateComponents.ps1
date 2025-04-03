e8df966e89c295058387fbe758ff4ffda2581c80
starfunkel <cra.memb@posteo.net>
2025-04-03 13:50:48 +0200

    mod for posh syntax

--- script_ideas/Repair-WindowsUpdateComponents.ps1
+++ script_ideas/Repair-WindowsUpdateComponents.ps1
@@ -4,8 +4,8 @@ net stop wuauserv
 net stop msiserver
 net stop cryptsvc
 net stop appidsvc
-Ren %Systemroot%\SoftwareDistribution SoftwareDistribution.old
-			Ren %Systemroot%\System32\catroot2 catroot2.old
+Rename-Item -Path C:\windows\SoftwareDistribution -NewName SoftwareDistribution.old
+Rename-Item -Path c:\windows\System32\catroot2 -NewName catroot2.old
 regsvr32.exe /s atl.dll
 regsvr32.exe /s urlmon.dll
 regsvr32.exe /s mshtml.dll
